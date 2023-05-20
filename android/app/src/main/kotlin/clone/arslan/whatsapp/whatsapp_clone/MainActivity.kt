ad30019684fe5a73f299c1ad8819eac842520ee5
KatayR <rasitarslan@outlook.com>
2023-05-20 11:55:59 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/clone/arslan/whatsapp/whatsapp_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package clone.arslan.whatsapp.whatsapp_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  void clickHandle(String value) {
+    switch (value) {
+      case 'Logout':
+        break;
+      case 'Settings':
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double tabWidth = width / 5;
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Mhatsapp'),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Icon(Icons.camera_enhance_outlined),
+                    ),
+                    Icon(Icons.search),
+                    PopupMenuButton<String>(
+                      onSelected: clickHandle,
+                      itemBuilder: (BuildContext context) {
+                        return {
+                          'New group',
+                          'New Broadcast',
+                          'Linked Devices',
+                          'Starred Messages',
+                          'Settings'
+                        }.map((String choice) {
+                          return PopupMenuItem<String>(
+                            value: choice,
+                            child: Text(choice),
+                          );
+                        }).toList();
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+            backgroundColor: Color(0xff128C7E),
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              indicatorSize: TabBarIndicatorSize.label,
+              isScrollable: true,
+              tabs: <Widget>[
+                Container(
+                  width: 30,
+                  height: 50,
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.people,
+                  ),
+                ),
+                Container(
+                    width: tabWidth,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: Text("Chats")),
+                Container(
+                    width: tabWidth,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: Text("Status")),
+                Container(
+                    width: tabWidth,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: Text("Calls"))
+              ],
+            ),
+          ),
+        ));
+  }
+}
