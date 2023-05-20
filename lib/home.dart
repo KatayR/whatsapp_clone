5a579216f45a89139a1f604d8f1ea6b5cebad478
KatayR <rasitarslan@outlook.com>
2023-05-20 15:02:24 +0300

    Completed "communities" screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/community.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -26,41 +27,39 @@ class _HomeState extends State<Home> {
     double width = MediaQuery.of(context).size.width;
     double tabWidth = width / 5;
     return DefaultTabController(
+        initialIndex: 1,
         length: 4,
         child: Scaffold(
+          backgroundColor: Colors.white,
           appBar: AppBar(
             title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Mhatsapp'),
-                Row(
-                  children: [
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Mhatsapp'),
+                  Row(children: [
                     Padding(
                       padding: const EdgeInsets.all(16.0),
                       child: Icon(Icons.camera_enhance_outlined),
                     ),
                     Icon(Icons.search),
                     PopupMenuButton<String>(
-                      onSelected: clickHandle,
-                      itemBuilder: (BuildContext context) {
-                        return {
-                          'New group',
-                          'New broadcast',
-                          'Linked devices',
-                          'Starred messages',
-                          'Settings'
-                        }.map((String choice) {
-                          return PopupMenuItem<String>(
-                            value: choice,
-                            child: Text(choice),
-                          );
-                        }).toList();
-                      },
-                    ),
-                  ],
-                )
-              ],
-            ),
+                        onSelected: clickHandle,
+                        itemBuilder: (BuildContext context) {
+                          return {
+                            'New group',
+                            'New broadcast',
+                            'Linked devices',
+                            'Starred messages',
+                            'Settings'
+                          }.map((String choice) {
+                            return PopupMenuItem<String>(
+                              value: choice,
+                              child: Text(choice),
+                            );
+                          }).toList();
+                        }),
+                  ])
+                ]),
             backgroundColor: Color(0xff128C7E),
             bottom: TabBar(
               indicatorColor: Colors.white,
@@ -75,24 +74,40 @@ class _HomeState extends State<Home> {
                     Icons.people,
                   ),
                 ),
-                Container(
-                    width: tabWidth,
-                    height: 50,
-                    alignment: Alignment.center,
-                    child: Text("Chats")),
-                Container(
-                    width: tabWidth,
-                    height: 50,
-                    alignment: Alignment.center,
-                    child: Text("Status")),
-                Container(
-                    width: tabWidth,
-                    height: 50,
-                    alignment: Alignment.center,
-                    child: Text("Calls"))
+                myTabs(tabWidth: tabWidth, label: 'Chats'),
+                myTabs(tabWidth: tabWidth, label: 'Status'),
+                myTabs(tabWidth: tabWidth, label: 'Calls'),
               ],
             ),
           ),
+          body: TabBarView(
+            children: [
+              Community(),
+              Icon(Icons.directions_transit, size: 350),
+              Icon(Icons.directions_car, size: 350),
+              Icon(Icons.directions_car, size: 350),
+            ],
+          ),
         ));
+  }
+}
+
+class myTabs extends StatelessWidget {
+  const myTabs({
+    super.key,
+    required this.tabWidth,
+    required this.label,
+  });
+
+  final double tabWidth;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: tabWidth,
+        height: 50,
+        alignment: Alignment.center,
+        child: Text(label));
   }
 }
--- lib/screens/community.dart
+++ lib/screens/community.dart
@@ -10,6 +10,51 @@ class Community extends StatefulWidget {
 class _CommunityState extends State<Community> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30),
+            child: Image.asset('assets/community/communities.png'),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              'Introducing communities',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Easily organize your related groups and send announcements. Now, your communities, like neighborhoods or schools, can have their own space.',
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(color: Colors.black45, fontSize: 19, height: 1.35),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: MaterialButton(
+                child: Text(
+                  'Start your community',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Color(0xff0b8d80),
+                elevation: 0,
+                height: 38,
+                shape: RoundedRectangleBorder(
+                  // Change your radius here
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                onPressed: () {
+                  print('pressed community button');
+                }),
+          )
+        ],
+      ),
+    );
   }
 }
