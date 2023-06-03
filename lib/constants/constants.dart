ca5f39affe1d5a4199cd1cfbb3915a35f9b79a31
KatayR <rasitarslan@outlook.com>
2023-06-03 21:22:04 +0300

    Added kUserPpURL

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -9,3 +9,5 @@ var kMarkReadIconGrey = Icon(Icons.done_all, size: 18);
 var kSentIconGrey = Icon(Icons.done, size: 18);
 var kTapToStatusMessage = "Tap to add status update";
 var kMyProfilePictureLocation = "assets/profilePicture/myPp.png";
+var kUserPpURL =
+    "https://firebasestorage.googleapis.com/v0/b/wapp-b6195.appspot.com/o/myPp.png?alt=media&token=d07f918f-fea8-4c8a-827a-2da6926f5c44";
