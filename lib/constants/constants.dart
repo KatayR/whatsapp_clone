84e1aff77328a0d7bf54932dfa5bfd19119b5141
KatayR <rasitarslan@outlook.com>
2023-06-05 21:36:45 +0300

    Cleanup

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,12 +2,12 @@
 
 import 'package:flutter/material.dart';
 
-var kTitlesStyle = TextStyle(fontWeight: FontWeight.w600, fontSize: 18);
-var kWhatsappGreen = Color(0xff128C7E);
-var kWhatsappGreenDark = Color(0xff0b8d80);
-var kMarkReadIconGrey = Icon(Icons.done_all, size: 18);
-var kSentIconGrey = Icon(Icons.done, size: 18);
-var kTapToStatusMessage = "Tap to add status update";
-var kMyProfilePictureLocation = "assets/profilePicture/myPp.png";
-var kUserPpURL =
+TextStyle kTitlesStyle = TextStyle(fontWeight: FontWeight.w600, fontSize: 18);
+Color kWhatsappGreen = Color(0xff128C7E);
+Color kWhatsappGreenDark = Color(0xff0b8d80);
+Icon kMarkReadIconGrey = Icon(Icons.done_all, size: 18);
+Icon kSentIconGrey = Icon(Icons.done, size: 18);
+String kTapToStatusMessage = "Tap to add status update";
+String kMyProfilePictureLocation = "assets/profilePicture/myPp.png";
+String kUserPpURL =
     "https://firebasestorage.googleapis.com/v0/b/wapp-b6195.appspot.com/o/myPp.png?alt=media&token=d07f918f-fea8-4c8a-827a-2da6926f5c44";
